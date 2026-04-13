0bea905eeab415a6d783e1b50e5a03d3109ac381
VALTER SERGIO RIBEIRO TERTULIANO <valtertert@gmail.com>
2026-04-12 21:50:10 -0300

    Compreendo elementos básicos de entrada e saída do C# Windows Forms (WinForms)

---
+++ Desenvolvimento-de-Sistemas/Exe01/Exe01/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Exe01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.inputNome = new System.Windows.Forms.TextBox();
+            this.inputSenha = new System.Windows.Forms.TextBox();
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.outputResposta = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(130, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Conecte-se";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(111, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nome";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(111, 172);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Senha";
+            // 
+            // inputNome
+            // 
+            this.inputNome.Location = new System.Drawing.Point(220, 114);
+            this.inputNome.Name = "inputNome";
+            this.inputNome.Size = new System.Drawing.Size(100, 20);
+            this.inputNome.TabIndex = 3;
+            // 
+            // inputSenha
+            // 
+            this.inputSenha.Location = new System.Drawing.Point(220, 172);
+            this.inputSenha.Name = "inputSenha";
+            this.inputSenha.PasswordChar = '*';
+            this.inputSenha.Size = new System.Drawing.Size(100, 20);
+            this.inputSenha.TabIndex = 4;
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConectar.Location = new System.Drawing.Point(116, 227);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(204, 50);
+            this.btnConectar.TabIndex = 5;
+            this.btnConectar.Text = "Conectar";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // outputResposta
+            // 
+            this.outputResposta.AutoSize = true;
+            this.outputResposta.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputResposta.Location = new System.Drawing.Point(122, 290);
+            this.outputResposta.Name = "outputResposta";
+            this.outputResposta.Size = new System.Drawing.Size(0, 17);
+            this.outputResposta.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 324);
+            this.Controls.Add(this.outputResposta);
+            this.Controls.Add(this.btnConectar);
+            this.Controls.Add(this.inputSenha);
+            this.Controls.Add(this.inputNome);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox inputNome;
+        private System.Windows.Forms.TextBox inputSenha;
+        private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.Label outputResposta;
+    }
+}
+
---
+++ Desenvolvimento-de-Sistemas/Exe01/Exe01/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exe01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            if (validarEntradas())
+            {
+                outputResposta.Text = "Seja bem-vindo " + inputNome.Text;
+                return;
+            }
+        }
+
+        private bool validarCampo(string campo)
+        {
+            return String.IsNullOrEmpty(campo);
+        }
+
+        private bool validarEntradas()
+        {
+            if (validarCampo(inputNome.Text))
+            {
+                outputResposta.Text = "Campo nome está vazio, tente novamente !!!";
+                return false;
+            }
+
+            if (validarCampo(inputSenha.Text))
+            {
+                outputResposta.Text = "Campo senha está vazio, tente novamente !!!";
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
